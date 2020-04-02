b092087a69086d45dd4d43abdc3ce3f3c2aa3bb1
Nagendra Singh Chauhan <nagendrasinghchauhan1@gmail.com>
2020-04-02 18:14:41 +0530

    Update covid19india.php

--- covid19india.php
+++ covid19india.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name:COVID19India
  * Plugin URI:https://github.com/nagu741/coronavirus
- * Description:Use shortcode [cti] to display COVID-19 India outbreak live Updates in your Page,post or widget section 
- * Author:Nagu741
- * Author URI:https://nagu741.github.io/
+ * Description:Use shortcode [cti] to display COVID-19 outbreak live Updates in your Page,post or widget section 
+ * Author:Nagendra Singh Chauhan
+ * Author URI:https://nagubaba.com/
  * License:MIT License
  * Version:0.1
  * Text Domain:cti
